c53ea4b9da96358b795e28cda1e9c6917ec1d1ea
Sara <saeraa@gmail.com>
2023-03-16 21:58:30 +0100

    first commit

---
+++ guessWord/guessWord.js
@@ -0,0 +1,48 @@
+const { hasSpecialCharsOrSpaces } = require("../utils/utils");
+
+module.exports = function guessWord(guessInput, correctWordInput) {
+	const result = [];
+
+	const guess = guessInput.toLowerCase();
+	const correctWord = correctWordInput.toLowerCase();
+
+	if (guess.length !== correctWord.length) {
+		return [{ error: "The word length does not match." }];
+	}
+
+	if (hasSpecialCharsOrSpaces(guess) || hasSpecialCharsOrSpaces(correctWord)) {
+		return [{ error: "Incorrect input." }];
+	}
+
+	const incorrectLetterGuesses = [];
+	const correctLetters = {};
+
+	for (let i = 0; i < guess.length; ++i) {
+		result[i] = { letter: guess[i], result: "incorrect" };
+		let targetLetter = correctWord[i];
+
+		if (targetLetter in correctLetters) {
+			correctLetters[targetLetter]++;
+		} else {
+			correctLetters[targetLetter] = 1;
+		}
+
+		if (guess[i] === targetLetter) {
+			result[i].result = "correct";
+			correctLetters[targetLetter]--;
+		} else {
+			incorrectLetterGuesses.push(i);
+		}
+	}
+
+	for (const i of incorrectLetterGuesses) {
+		let guessLetter = guess[i];
+
+		if (guessLetter in correctLetters && correctLetters[guessLetter] > 0) {
+			result[i].result = "misplaced";
+			correctLetters[guessLetter]--;
+		}
+	}
+
+	return result;
+};
---
+++ utils/utils.js
@@ -0,0 +1,23 @@
+function hasOnlyUniqueLetters(str) {
+	for (let i = 0; i < str.length; i++) {
+		if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function hasSpecialCharsOrSpaces(str) {
+	const regex = /[\s~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/;
+
+	if (str.match(regex)) {
+		return true;
+	}
+
+	return false;
+}
+
+module.exports = {
+	hasOnlyUniqueLetters,
+	hasSpecialCharsOrSpaces
+};
